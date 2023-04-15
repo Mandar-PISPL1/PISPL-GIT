3c4405d987ddef700966bb5a96a6987c60e29992
Adarsshh34 <adarshkamble130@gmail.com>
2023-04-15 14:14:17 +0530

    few changes

--- pispl/src/components/Services/translation/Translation.js
+++ pispl/src/components/Services/translation/Translation.js
@@ -4,8 +4,8 @@ import Banner from '../../translation/Banner'
 import translation_service from './translation_service.json'
 import TopBrandSection from '../../translation/TopBrandSection'
 import brands from './brands'
-
-
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { ImCheckmark  } from "react-icons/im";
 export default function Translation() {
 
   const mystyle = {
@@ -14,10 +14,24 @@ export default function Translation() {
     flexDirection: "row",
     flexWrap: "wrap"
   }
+  const buttonstyle={
+    color:"#0d2366",
+    backgroundColor:"#49dab5",
+    fontSize:"20px",
+    width:"19rem",
+    height:"3rem",
+    borderRadius:"5px",
+    border:"0px",
+    margin:"0px auto",
+    display:" block",
+    margin: "0 auto",
+}
+  
 
   return (
     <>
       <Banner index={0} a={translation_service} />
+      {/* Top Brands Section */}
       <div className="repon" style={{ textAlign:"center",margin:"3rem auto",textAlign: "center"}}>
         <h1 className='text-center m-3' style={{ color: "#0d2366", fontWeight: "700" }}>Brands We Work With</h1>
         <div className='container-fluid'>
@@ -31,6 +45,18 @@ export default function Translation() {
             }
           </div>
         </div>
+      </div>
+      {/* End of Top Brands Section */}
+
+      {/* USP section */}
+      <div style={{backgroundColor:"#0d2366"}}>
+        <h1  className="text-center my-3" style={{fontWeight: "700",color:"white"}}>Get Your Effective Translation Today!</h1>
+        <div className='text-white d-flex justify-content-center my-3 usp-section'>
+          <p><ImCheckmark color='#49dab5'size="1.4rem"/>Multiple languages covered</p>
+          <p><ImCheckmark color='#49dab5' size="1.4rem"/>Per Word Rate Structure</p>
+          <p><ImCheckmark color='#49dab5' size="1.4rem"/>On-Time Delivery</p>
+        </div>
+        <button style={buttonstyle}><a>Start Translating <HiArrowNarrowRight/></a></button>
       </div>
 
     </>
