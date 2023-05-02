9b9ad337567cb31105ea0d675f44b01eeb4c0800
Adarsh <web@parikhinfosolutions.com>
2023-05-02 16:57:31 +0530

    benefit section in dubbing

---
+++ pispl/src/components/Services/dubbing/BenefitsContent.js
@@ -0,0 +1,18 @@
+const arr=[
+    {
+        title:"Quality Oriented",
+        img:"https://parikhinfosolutions.com/wp-content/uploads/2020/10/Quick-Interpretation.png",
+        content:"At Parikh Info Solutions, we offer high-quality professional dubbing services in USA as well all other countries across industries like Entertainment- movies, TV-series, animation, and documentaries, eLearning, Corporate, Gaming, Advertising, and many more.",
+    },
+    {
+        title:"Hassle-Free Process",
+        img:"https://parikhinfosolutions.com/wp-content/uploads/2020/10/network.png",
+        content:"Parikh Info Solutions commits to serve you a hassle-free single point of contact for professional dubbing services in many national, local & international languages while maintaining a good dubbing studio quality.",
+    },
+    {
+        title:"Cost-Effective",
+        img:"https://parikhinfosolutions.com/wp-content/uploads/2020/10/Alerts-for-Cultural-Sensitivity-1.png",
+        content:"Our highly qualified and experienced team of native linguists, project managers, voice over artists, and dubbing artists are committed to delivering high-quality video dubbing services at cost effective & pocket friendly rates while preserving the authenticity & retaining the tone of the content.",
+    },
+];
+ export default arr;
--- pispl/src/components/Services/dubbing/Dubbing.js
+++ pispl/src/components/Services/dubbing/Dubbing.js
@@ -6,6 +6,8 @@ import TopBrandSection from '../../translation/TopBrandSection'
 import UspSection from '../../translation/UspSection'
 import dubbing_lottie from './dubbing_lottie.json'
 import { useEffect } from 'react';
+import Benefits from '../../translation/Benefits';
+import arr from './BenefitsContent'
 export default function Dubbing() {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -32,6 +34,25 @@ export default function Dubbing() {
             }
           </div>
         </div>
+      </div>
+      {/* benefits content */}
+      <div className='container py-4 mx-md-2'>
+        <h2 className='text-center m-3' style={{ color: "#0d2366", fontWeight: "700" }} >Why Choose Parikh Info Solutions?</h2>
+        <p className='px-md-5 mx-md-5 m-0 text-center' style={{color:"#2a3d78"}}>Give Your Viewers A Better Experience with us</p>
+        <div className='row px-md-5 mx-md-5 m-0'>
+        {
+          arr.map((element) =>{
+            return (
+             
+                <Benefits img={element.img} title={element.title} content={element.content}/>
+              
+            )
+          })
+        }
+
+
+        </div>
+
       </div>
       <UspSection
         heading="Give Your Viewers A Better Experience"
