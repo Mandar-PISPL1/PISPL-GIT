cb322eaeb42a73663a1b4f93500e4e91cbe95483
Adarsshh34 <adarshkamble130@gmail.com>
2023-05-23 17:29:40 +0530

    what is localization-localization pg!

--- pispl/src/components/Services/localization/Localization.js
+++ pispl/src/components/Services/localization/Localization.js
@@ -8,6 +8,7 @@ import UspSection from '../../translation/UspSection'
 import { useEffect } from 'react';
 import Benefits from '../../translation/Benefits';
 import arr from './BenefitsContent'
+import img from '../../assets/Software-Localization.png'
 export default function Localization() {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -20,9 +21,27 @@ export default function Localization() {
   }
   return (
     <>
-        <Banner index={2} lottie={localization_lottie}/>
-        <div className='myskew'></div>
-        <div className="repon" style={{ textAlign: "center", margin: " 3rem auto"}}>
+      <Banner index={2} lottie={localization_lottie} />
+      <div className='myskew'></div>
+
+      {/* What is localization */}
+      <div className="conatiner py-4 mx-md-2">
+        <div className="row px-md-5 mx-md-5 m-0">
+          <h1 className='text-center m-3 ml-2' style={{ color: "#0d2366", fontWeight: "700" }}>What is Localization?</h1>
+          <div className="col-md-6 col-12">
+            <p className='text-start m-3' style={{ color: "#0d2366", fontWeight: "300" }} >Localization is the process of modifying a product to adapt to the needs of the audience in different regions. While translation services are often a key component, language localization includes local regulations and cultural differences in order to connect with the intended audience.<br /><br/>
+
+              The true skill of translating and localizing a document lies in adapting it to a format without changing its true meaning.<br /><br/>
+
+              We offer the best localization services in India at affordable prices and fast outcomes.</p>
+
+          </div>
+          <div className="col-md-6 col-12 text-center">
+            <img src={img} alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="repon" style={{ textAlign: "center", margin: " 3rem auto" }}>
         <h1 className='text-center m-3' style={{ color: "#0d2366", fontWeight: "700" }}>Top Brands</h1>
         <div className='container-fluid'>
           <div className='' style={mystyle}>
@@ -40,15 +59,15 @@ export default function Localization() {
       <div className='container py-4 mx-md-2'>
         <h2 className='text-center m-3' style={{ color: "#0d2366", fontWeight: "700" }} >How Language Localization Services Companies Will Benefit?</h2>
         <div className='row px-md-5 mx-md-5 m-0'>
-        {
-          arr.map((element) =>{
-            return (
-             
-                <Benefits img={element.img} title={element.title} content={element.content}/>
-              
-            )
-          })
-        }
+          {
+            arr.map((element) => {
+              return (
+
+                <Benefits img={element.img} title={element.title} content={element.content} />
+
+              )
+            })
+          }
 
 
         </div>
@@ -62,10 +81,10 @@ export default function Localization() {
         data2="Native Linguists"
         data3="Higher Search Rate"
         button="Localize Now"
-        />
-        <div style={{backgroundColor:"#49dab5"}}>
-                    <p className='text-white text-center my-0 py-3 fs-6'>Are you looking for language interpreter? <NavLink to="/interpretation">Read More.</NavLink></p>
-        </div>
+      />
+      <div style={{ backgroundColor: "#49dab5" }}>
+        <p className='text-white text-center my-0 py-3 fs-6'>Are you looking for language interpreter? <NavLink to="/interpretation">Read More.</NavLink></p>
+      </div>
     </>
   )
 }
