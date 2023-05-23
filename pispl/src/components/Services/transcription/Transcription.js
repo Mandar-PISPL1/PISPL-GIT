0b69aa75ad4e0523f7e8e44960c0e04b413c61fa
Adarsshh34 <adarshkamble130@gmail.com>
2023-05-23 17:20:54 +0530

    Transform Your Multimedia to Defi.- transcripiton!

--- pispl/src/components/Services/transcription/Transcription.js
+++ pispl/src/components/Services/transcription/Transcription.js
@@ -21,6 +21,20 @@ export default function Transcription() {
     <>
       <Banner index={1} lottie={transcription_lottie} />
       <div className='myskew'></div>
+      {/* Transform Your Multimedia to Definitive Text */}
+      <div className="conatiner py-4 mx-md-2">
+        <div className="row px-md-5 mx-md-5 m-0">
+          <div className="col-md-7 col-12">
+            <p className='text-start m-3' style={{ color: "#0d2366", fontWeight: "300" }} >WE WRITE THE WORDS YOU SPEAK!</p>
+            <h1 className='text-start m-3 ml-2' style={{ color: "#0d2366", fontWeight: "700" }}>Transform Your Multimedia to Definitive Text</h1>
+            <div className='my-4 ' style={{ width: "40px", backgroundColor: "#49dab5", height: "5px" }}></div>
+            <p className='text-start m-3' style={{ color: "#0d2366", fontWeight: "300" }} >Our team of professional transcribers ensures that the quality and essence of your files are retained. You can expect unparalleled, authentic texts; translated into a multitude of languages for widespread use. Be it any language, our experts pen down every sound, every word with precision.</p>
+          </div>
+          <div className="col-md-5 col-12">
+          <iframe width="456" height="258" src="https://www.youtube.com/embed/ygJWdNwsyms" title="Budget Friendly Transcription Services in India - Parikh Info Solutions" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+        </div>
+      </div>
       {/* Top Brand Section */}
       <div className="repon" style={{ textAlign: "center", margin: " 3rem auto" }}>
         <h1 className='text-center m-3' style={{ color: "#0d2366", fontWeight: "700" }}>Join Our Happy Customers</h1>
