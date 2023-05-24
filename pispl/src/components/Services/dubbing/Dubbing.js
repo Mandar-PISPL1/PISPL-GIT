f75406ef1857cb1f91e18c19049ca5008a1d6d13
Adarsh <web@parikhinfosolutions.com>
2023-05-24 17:37:13 +0530

    Adapt To The Local Culture - dubbing pg!

--- pispl/src/components/Services/dubbing/Dubbing.js
+++ pispl/src/components/Services/dubbing/Dubbing.js
@@ -8,6 +8,8 @@ import dubbing_lottie from './dubbing_lottie.json'
 import { useEffect } from 'react';
 import Benefits from '../../translation/Benefits';
 import arr from './BenefitsContent'
+import img from '../../assets/Dubbing-2.png'
+import img2 from '../../assets/Simultaneous-Interpretation.png'
 export default function Dubbing() {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -22,6 +24,58 @@ export default function Dubbing() {
     <>
       <Banner index={4} lottie={dubbing_lottie} />
       <div className='myskew'></div>
+
+      {/* We entertain all major languages across the globe! */}
+
+      <div className="container py-4 mx-md-2">
+        <div className="row px-md-5 mx-md-5 m-0">
+
+          <div className="col-md-7 col-12">
+            <p style={{ color: "#0d2366", fontWeight: "700" }}>ADAPT TO THE LOCAL CULTURE</p>
+            <h1 className=' m-3 ml-2' style={{ color: "#0d2366", fontWeight: "700" }}>We entertain all major languages across the globe!</h1>
+            <div className='my-4 ' style={{ width: "40px", backgroundColor: "#49dab5", height: "5px" }}></div>
+            <p className='text-start m-3' style={{ color: "#0d2366", fontWeight: "300" }} >
+            Dubbing is a part of the post-production process that involves the synchronization of the new sound over the original sound dialogue to make it accessible for local audiences. Being an important part of the video that has a significant impact on the audience, it becomes crucial to dub the voice in a professional manner.<br /><br />
+
+            While some audiences prefer other solutions like subtitles, closed captions, etc, the majority of the audience prefer dubbing. By providing the best dubbing services in India, Parikh info solutions already stands strong in the industry.<br /><br />
+
+            With expertise in a number of languages, nationally & internationally, Parikh info solutions levels up the game of dubbing services in India as well as other countries.<br /><br />
+
+            Dubbing not only localizes the voice for the audience but also takes care of translating the emotions that the original sound conveys.<br /><br />
+            It adapts the authenticity that the original form consists of, translates it and offers it to the audience without dilution. With native expertise in the field, as a leading dubbing services provider globally, at Parikh info solutions we take care of the accuracy, reliability & relevance of the data.<br /><br />
+            With a strong network of native linguists and experts to complete the job with dedication, we also take care of the synchronization, acting & narration important to translate the emotions along with ensuring the perfect timing, sound quality & best quality of all the dubbed voice overs.<br /><br />
+            </p>
+
+          </div>
+          <div className="col-md-5 col-12 text-center" style={{maxHeight:"456px",maxWidth:"456px"}}>
+            <img src={img} alt="" style={{maxHeight:"100%",maxWidth:"100%"}}/>
+          </div>
+        </div>
+      </div>
+
+      {/* Adapt To The Local Culture */}
+      <div className="conatiner py-4 mx-md-2">
+        <div className="row px-md-5 mx-md-5 m-0">
+          <h1 className='text-center m-3 ml-2' style={{ color: "#0d2366", fontWeight: "700" }}>Adapt To The Local Culture</h1>
+          <p className='text-start m-3' style={{ color: "#0d2366", fontWeight: "300" }}>We entertain all major languages across the globe!</p>
+          <div className="col-md-6 col-12">
+            <p className='text-start m-3' style={{ color: "#0d2366", fontWeight: "300" }} >We deliver the best dubbing services in every aspect. We make sure the dubbing artist suits the original speaker. Whether it may be the age, gender, tone, pitch, and depth of voice; all such criteria undergo strict inspection. At most competitive rates, our veteran dubbing artists never fail to convey emotion-filled dialogues..<br /><br />
+
+            We cover world languages to offer dubbing services in all major Indian languages, Asian languages, European languages and African languages. Without confining to only mainstream languages, we also offer our services in local languages that can encourage business in remote local areas.</p>
+
+          </div>
+          <div className="col-md-6 col-12 text-center" >
+            <img src={img2} alt="" style={{ MaxHeight: "200px", maxWidth: "315px" }}/>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
       <div className="repon" style={{ textAlign: "center", margin: " 3rem auto"}}>
         <h1 className='text-center m-3' style={{ color: "#0d2366", fontWeight: "700" }}>Top Brands</h1>
         <div className='container-fluid'>
