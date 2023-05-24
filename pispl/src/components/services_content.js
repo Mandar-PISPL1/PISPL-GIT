388e3522a621beb81250812e735fe4c8e959b1bf
Mandar-PISPL1 <mandar@parikhinfosolutions.com>
2023-05-24 17:47:14 +0530

    Services H1 Content Updated

--- pispl/src/components/services_content.js
+++ pispl/src/components/services_content.js
@@ -1,6 +1,6 @@
 const arr=[
     {
-        name_of_service:"Translation Services in India",
+        name_of_service:"Certified Language Translation Services",
         content1:"Make your business multilingual, reach out to your audience in their native language through our professional language translation services.",
         content2:"",
         content3:""
@@ -8,14 +8,14 @@ const arr=[
         
     },
     {
-        name_of_service:"Transcription Services in India",
+        name_of_service:"Professional Transcription Services - Audio & Video",
         content1:"In a world full of software-generated inaccurate texts, we are here to lessen your burden by transcribing your audios and video files into written texts with utmost professionalism and 99% accuracy level. We take up full human transcription as well as QC of auto-generated transcriptions by our experienced transcribers as per your requirement, budget, content type, audio quality and urgency.",
         content2:"",
         content3:"",
 
     },
     {
-        name_of_service: "Localization Services In India",
+        name_of_service: "Professional Localization Service Provider",
         content1: "With years dedicated to the localization industry, we have perfected the formula to create decisive content and methods for localization. Be it Apps, Websites, or Software, we help you connect with consumers on the most fundamental level.",
         content2: "",
         content3: ""
@@ -23,21 +23,21 @@ const arr=[
     
     ,
     {
-        name_of_service:"Voice Over Services in India",
+        name_of_service:"Professional Voice Over Service",
         content1:"Relish the masterful narrations, describing your ideas, in the voice of local communities.",
         content2:"We represent a professional voice over services offering company, offering premium voice-overs and narrations, bestowing the linguistic edge to your campaigns and commercials.",
         content3:""
 
     },
     {
-        name_of_service:"Dubbing Services",
+        name_of_service:"Professional Dubbing Services For Global Reach",
         content1:"Be it script translation, voice-over, transcription services, cultural specifics or directing, dialogue recording and post-production, we cover everything. All your dubbing needs are fulfilled, under a single umbrella by our dubbing services.",
         content2:"Give your platforms a conviction of trust and sincerity, with our excellent services. With decades of experience, as a leading Dubbing services provider, we at Parikh info solutions aim to make the process easier for our clients.",
         content3:""
 
     },
     {
-        name_of_service:"AI Dubbing - Powered by Humans",
+        name_of_service:"AI Dubbing with Human-Like Voice in 25+ Languages",
         content1:"The ultimate blend of AI dubbing under human expertise to produce error-free, accurate, reliable & quality voice-over solutions!",
         content2:"",
         conten3:""
@@ -45,28 +45,28 @@ const arr=[
 
     },
     {
-        name_of_service:"Video Subtitling Services",
+        name_of_service:"Best Video Subtitling & Captioning Services Online",
         content1:"We understand the rhythm of the scenes, and how seamlessly the subtitles should be positioned with them. In our video subtitling services advanced software, perfect time sync, and motion graphics are assured.",
         content2:"A quick and smart solution for all genres of multimedia. We have one of the most assorted linguistic support for movies, TV shows, web series, marketing videos, e-learning, stage performances in all the mellifluous tongues.",
         content3:"Get the best-localized subtitling services in India with cultural sensitivities for all your media!"
 
     },
     {
-        name_of_service:"Video Remote Interpretation Services",
+        name_of_service:"Best Video Remote Interpretation Service Providers",
         content1:"Connect Better With Us for Best Video Remote Interpretation Services in India.",
         content2:"",
         conten3:""
 
     },
     {
-        name_of_service:"Content Writing Services in India",
+        name_of_service:"Best SEO Friendly Content Writing Services",
         content:"Get Noticed with Precise, Professional & Quality-Driven Content!",
         content2:"We can deliver all your technical, digital and professional needs with our bespoke content writing services. Our team of digital-savvy experts & skilled writers will help you hit the target audience with the right words & highly optimized SEO content.",
         content3:""
 
     },
     {
-        name_of_service:"Explainer Video Creation Services in India",
+        name_of_service:"Best Video Creation and Editing Services",
         content1:"Let the Video Speak for your Brand!",
         content2:"Capture your audiences attention with creative and professional explainer videos. With an increase in technological advancements, people have become prone to short attention span time. The explainer videos can help you spread your brands message and spread awareness effectively in a few minutes. We at Parikh Info Solutions, offer video creation services at every step from content, scripting, and editing, to voice-over and final video production.",
         content3:""
