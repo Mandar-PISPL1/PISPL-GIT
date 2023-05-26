20593574bfc6d5930614e971a58ae00101ac3250
Adarsh <web@parikhinfosolutions.com>
2023-05-26 13:02:03 +0530

    Connect Better With Us -interpretation

--- pispl/src/components/Services/interpretation/Interpretation.js
+++ pispl/src/components/Services/interpretation/Interpretation.js
@@ -13,6 +13,26 @@ export default function () {
     <>
         <Banner index={7} lottie={interpretation_lottie}/>
         <div className='myskew'></div>
+
+        {/* Connect Better With Us! */}
+
+        <div className="conatiner py-4 mx-md-2">
+        <div className="row px-md-5 mx-md-5 m-0">
+          <div className="col-md-7 col-12">
+            <p className='text-start m-3' style={{ color: "#0d2366", fontWeight: "300" }} >GET PROFESSIONAL INTERPRETERS ANYTIME, ANYWHERE!</p>
+            <h1 className='text-start m-3 ml-2' style={{ color: "#0d2366", fontWeight: "700" }}>Connect Better With Us!</h1>
+            <div className='my-4 ' style={{ width: "40px", backgroundColor: "#49dab5", height: "5px" }}></div>
+            <p className='text-start m-3' style={{ color: "#0d2366", fontWeight: "300" }} >Our interpreters in India operate in an interruption-free environment with a professional background. We are committed to maximizing the quality of your communication experience through a zero disturbance policy.
+
+
+            </p>
+          </div>
+          <div className="col-md-5 col-12" style={{ maxHeight: "456px", maxWidth: "456px" }}>
+            <img src={img} alt="voice over" style={{ height: "100%", width: '100%' }}></img>
+          </div>
+        </div>
+      </div>
+
         {/* Benefits content */}
         <div className='container py-4 mx-md-2'>
         <h2 className='text-center m-3' style={{ color: "#0d2366", fontWeight: "700" }} >Industries We Are Working With</h2>
