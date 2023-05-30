43ea978682e469b4b27f55de5b4286435bde7547
Adarsh <web@parikhinfosolutions.com>
2023-05-30 17:33:41 +0530

    some style added in transcription pg!

--- pispl/src/components/Services/transcription/Transcription.js
+++ pispl/src/components/Services/transcription/Transcription.js
@@ -28,10 +28,10 @@ export default function Transcription() {
       <div className="conatiner py-4 mx-md-2">
         <div className="row px-md-5 mx-md-5 m-0">
           <div className="col-md-7 col-12">
-            <p className='text-start m-3' style={{ color: "#0d2366", fontWeight: "300" }} >WE WRITE THE WORDS YOU SPEAK!</p>
-            <h1 className='text-start m-3 ml-2' style={{ color: "#0d2366", fontWeight: "700" }}>Transform Your Multimedia to Definitive Text</h1>
-            <div className='my-4 ' style={{ width: "40px", backgroundColor: "#49dab5", height: "5px" }}></div>
-            <p className='text-start m-3' style={{ color: "#0d2366", fontWeight: "300" }} >Our team of professional transcribers ensures that the quality and essence of your files are retained. You can expect unparalleled, authentic texts; translated into a multitude of languages for widespread use. Be it any language, our experts pen down every sound, every word with precision.</p>
+            <p className='text-start mx-3 my-0' style={{ color: "#0d2366", fontWeight: "300" }} >WE WRITE THE WORDS YOU SPEAK!</p>
+            <h1 className='text-start mx-3 ml-2' style={{ color: "#0d2366", fontWeight: "700" }}>Transform Your Multimedia to Definitive Text</h1>
+            <div className='my-4 mx-3' style={{ width: "40px", backgroundColor: "#49dab5", height: "5px" }}></div>
+            <p className='text-start m-3 normal-text'  >Our team of professional transcribers ensures that the quality and essence of your files are retained. You can expect unparalleled, authentic texts; translated into a multitude of languages for widespread use. Be it any language, our experts pen down every sound, every word with precision.</p>
           </div>
           <div className="col-md-5 col-12">
             <iframe width="456" height="258" src="https://www.youtube.com/embed/ygJWdNwsyms" title="Budget Friendly Transcription Services in India - Parikh Info Solutions" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
@@ -39,10 +39,10 @@ export default function Transcription() {
         </div>
       </div>
       {/* Top Brand Section */}
-      <div className="repon" style={{ textAlign: "center", margin: " 3rem auto" }}>
-        <h1 className='text-center m-3' style={{ color: "#0d2366", fontWeight: "700" }}>Join Our Happy Customers</h1>
+      <div className="repon py-3 px-md-5" style={{ textAlign: "center", margin: " 3rem auto",backgroundColor:"#f6f6f6" }}>
+        <h1 className='text-center m-5' style={{ color: "#0d2366", fontWeight: "700" }}>Join Our Happy Customers</h1>
         <div className='container-fluid'>
-          <div className='' style={mystyle}>
+          <div className='mb-5' style={mystyle}>
             {
               brands.map((element) => {
                 return (
@@ -58,7 +58,7 @@ export default function Transcription() {
 
       {/* Benefits */}
       <div className='container py-4 mx-md-2'>
-        <h2 className='text-center m-3' style={{ color: "#0d2366", fontWeight: "700" }} >Benefits of Our Transcription Services</h2>
+        <h2 className='text-center m-3 heading-text' style={{ color: "#0d2366", fontWeight: "700" }} >Benefits of Our Transcription Services</h2>
         <div className='row px-md-5 mx-md-5 m-0'>
           {
             arr.map((element) => {
@@ -76,15 +76,15 @@ export default function Transcription() {
       </div>
       <div className="conatiner py-4 mx-md-2">
         <div className="row px-md-5 mx-md-5 m-0">
-        <h2 className='text-center m-3' style={{ color: "#0d2366", fontWeight: "700" }} >Get Your Transcription Done in 4 Easy Steps</h2>
-        <p className='text-center m-3' style={{ color: "#0d2366", fontWeight: "300" }} >With skilled and proficient transcribers, you can expect a comprehensive breakdown of the entire audio/video file into a cognitive language.</p>
+        <h2 className='text-center m-3 heading-text' style={{ color: "#0d2366", fontWeight: "700" }} >Get Your Transcription Done in 4 Easy Steps</h2>
+        <p className='text-center m-3 normal-text' >With skilled and proficient transcribers, you can expect a comprehensive breakdown of the entire audio/video file into a cognitive language.</p>
         </div>
 
       </div>
 
       <div style={{ height: "7rem", width: "100%", zIndex: "-1", transform: "skew(1deg,-2.9deg)", transformOrigin: "left top", position: "relative", backgroundColor: "#0d2366", top: "7rem" }}></div>
       <div className="conatiner-fluid py-4 " style={{ backgroundColor: "#0d2366" }}>
-        <div className="row px-md-5 mx-md-5 m-0">
+        <div className="row px-md-5 mx-md-5 m-0 align-items-center">
           
 
           <div className="col-md-3 col-12 " style={{ margin: "19px 0px" }}>
@@ -95,23 +95,23 @@ export default function Transcription() {
           </div>
 
           <div className='col-md-3 col-12' style={{ margin: "19px 0px" }}>
-            <div className=" d-flex flex-column rounded-5" style={{ backgroundColor: "white", width: "17rem", margin: "0 auto" }}>
+            <div className=" d-flex flex-column rounded-5" style={{ backgroundColor: "white", width: "17rem", margin: "0 auto",height:"15rem",justifyContent:"center" }}>
               <h2 className='h1 text-center' style={{ color: "#49dab5", fontSize: "70px" }}>2</h2>
               <p className='text-center'>Transcription (Human or MT+Human) with/without time coding</p>
             </div>
           </div>
           <div className='col-md-3 col-12' style={{ margin: "19px 0px" }}>
 
-            <div className=" d-flex flex-column rounded-5" style={{ backgroundColor: "white", width: "17rem", margin: "0 auto" }}>
+            <div className=" d-flex flex-column rounded-5" style={{ backgroundColor: "white", width: "15rem", margin: "0 auto" ,height:"10rem",justifyContent:"center" }}>
               <h2 className='h1 text-center' style={{ color: "#49dab5", fontSize: "70px" }}>3</h2>
               <p className='text-center'>Proofreading / QA</p>
             </div>
           </div>
 
           <div className='col-md-3 col-12' style={{ margin: "19px 0px" }}>
-            <div className=" d-flex flex-column rounded-5" style={{ backgroundColor: "white", width: "17rem", margin: "0 auto" }}>
+            <div className=" d-flex flex-column rounded-5" style={{ backgroundColor: "white", width: "17rem", margin: "0 auto",height:"15rem",justifyContent:"center" }}>
               <h2 className='h1 text-center' style={{ color: "#49dab5", fontSize: "70px" }}>4</h2>
-              <p className='text-center'>Finalize Transcription as per client specifications</p>
+              <p className='text-center px-2'>Finalize Transcription as per client specifications</p>
             </div>
           </div>
         </div>
@@ -127,7 +127,7 @@ export default function Transcription() {
         button="Get Your Transcription"
       />
       <div style={{ backgroundColor: "#49dab5" }}>
-        <p className='text-white text-center my-0 py-3 fs-6'>Are you looking for professional translation services company? <NavLink to="/translation">Read More.</NavLink></p>
+        <p className='text-white text-center my-0 py-3 fs-6'>Are you looking for professional translation services company? <NavLink to="/translation" style={{color:"#0d2366"}}><u>Read More.</u></NavLink></p>
       </div>
 
       <div className='container p-5'>
